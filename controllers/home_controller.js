2b40b4321c53c56cabd87d7ee8d0001974171c23
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-12-01 14:27:19 +0530

    created post schema form and auth

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,26 @@
+const Post = require('../models/posts');
+
+//! by pouplating u means e=means going one step inside of user we can ascess its properties
 module.exports.home = function (req, res) {
-    res.render('home');
+    Post.find({}).populate('user').exec(function (err, post) {
+        if (err) {
+            console.log("error in finding post", err);
+            return;
+        }
+        return res.render('home', {
+            posts: post,
+        }) 
+    })
 }
+//! by this we get only the user id
+// module.exports.home = function (req, res) {
+//     Post.find({}, function (err, post) {
+//         if (err) {
+//             console.log("error in finding post", err);
+//             return;
+//         }
+//         return res.render('home', {
+//             posts: post,
+//         })
+//     })
+// }
