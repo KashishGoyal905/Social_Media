c952a91c1941daf98a47a2aed941fa2f3e67a2ea
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-12-02 19:08:20 +0530

    added comment functionality

---
+++ controllers/comment_controller.js
@@ -0,0 +1,23 @@
+const Comment = require('../models/comments');
+const Post = require('../models/posts');
+
+module.exports.create = function (req, res) {
+    Post.findById(req.params.id, function (err, post) {
+        if (post) {
+            Comment.create({
+                content: req.body.content,
+                user: req.user._id,
+                post: req.params.id,
+            }, function (err, comment) {
+                if (err) {
+                    console.log("error in creating comment");
+                    return;
+                }
+                console.log(comment);
+                post.comments.push(comment);
+                post.save();
+                res.redirect('back');
+            });
+        }
+    })
+}
